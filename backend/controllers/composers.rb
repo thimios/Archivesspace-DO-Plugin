1e2ac638c703a09b1d0e30f5d936b4e8cbf747de
jambun <jambun@gmail.com>
2016-09-07 12:43:04 +1000

    simple html view for detailed

--- backend/controllers/composers.rb
+++ backend/controllers/composers.rb
@@ -1,3 +1,5 @@
+require_relative '../../../../common/mixed_content_parser'
+
 class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.get('/plugins/composers/summary')
@@ -17,17 +19,28 @@ class ArchivesSpaceService < Sinatra::Base
 
 
   Endpoint.get('/plugins/composers/detailed')
-         .description("Get detailed Digital Object data for a specific Resource")
-         .params(["component_id", String])
+         .description("Get detailed data for a specific digital object record")
+         .params(["component_id", String, "Component id for the record"],
+                 ["format", String, "Format of the data returned - json(default), html", :optional => true])
          .permissions([])
     .returns([200, "[(:digital_object)]"],
              [400, :error]) \
   do
-    resp = Composers.detailed(params[:component_id])
-    if resp.empty?
-      json_response({:error => "Object not found for component id: #{params[:component_id]}"}, 400)
+    format = params.fetch(:format) { 'json' }
+
+    unless ['json', 'html'].include?(format)
+      json_response({:error => "Unrecognized format: #{format}. Must be 'json' or 'html'"}, 400)
     else
-      json_response(resp)
+      record = Composers.detailed(params[:component_id])
+      if record.empty?
+        json_response({:error => "Object not found for component id: #{params[:component_id]}"}, 400)
+      else
+        if format == 'html'
+          ERB.new(File.read('../plugins/composers/backend/views/detail.html.erb')).result(binding)
+        else
+          json_response(record)
+        end
+      end
     end
   end
 
